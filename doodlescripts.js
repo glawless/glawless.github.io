493f95af5883caae8d29ceee2e2e7cbc867b4677
glawless <glawless@users.noreply.github.com>
2020-06-08 16:46:28 -0700

    Moving images

--- doodlescripts.js
+++ doodlescripts.js
@@ -1,9 +1,21 @@
-console.log('OK');
-
-
 gsap.from(
-  '#heart1', {
+  '#heart', {
     opacity: 0,
-    duration: 5, 
-    y: -100
+    duration: 2,
+    transform: 0,
+    y: -300
   });
+
+  gsap.from(
+    '#heart2', {
+      opacity: 0,
+      duration: 2,
+      y: -300
+    });
+
+    gsap.from(
+      '#bone', {
+        opacity: 0,
+        duration: 2,
+        x: 300
+      });
